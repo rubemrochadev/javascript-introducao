41c9632a742f173ac48498011d2790f84e42fa75
rubemrochadev <rubemrocha.dev@gmail.com>
2025-04-24 16:57:56 -0300

    exercicio swith-case

---
+++ atividades-switch-case.js.js
@@ -0,0 +1,63 @@
+// let pontoCardeal = "norte";
+
+// switch (pontoCardeal) {
+//     case "norte":
+//         console.log("Pra cima");
+//         break;
+//     case "sul":
+//         console.log("Pra baixo");
+//         break;
+//     case "leste":
+//         console.log("Pra direita");
+//         break;
+//     case "oeste":
+//         console.log("Pra esquerda");
+//         break;
+//     default:
+//         console.log("Informe uma opção válida!");
+// }
+
+//Exercícios para resolver com switch/case 
+
+// 1 - Dia da Semana:Peça um nú mero de 1 a 7 e exiba o dia correspondente (1 - Domingo, 2 - Segunda...).
+
+let diaDaSemana = 1;
+switch (diaDaSemana) {
+    case 1 
+    console.log(Domingo);
+    "Domingo";        
+        break;
+    case value: 2 "Segunda"        
+        break;
+    case value: 3 "Terça"        
+        break;
+    case value: 4 "Quarta"        
+        break;
+    case value: 5 "Quinta"        
+        break;
+    case value: 6 "Sexta"        
+        break;
+    case value: 7 "Sábado"        
+        break;
+    
+    default:
+        break;
+}
+
+//2 - Nota por Conceito: Peça uma nota (A, B, C, D ou F) e exiba o conceito correspondente ("Ótimo", "Bom", "Regular", "Ruim", "Reprovado").
+
+//3 - Calculadora Simples:Peça dois números e uma operação matemática (+, -, *, /) e exiba o resultado da operação.
+
+//4 - Mês do Ano:Peça um número de 1 a 12 e exiba o nome do mês correspondente.
+
+//5 - Classificação de Filme:Peça uma classificação etária (L, 10, 12, 14, 16, 18) e exiba a faixa etária permitida para assistir ao filme.
+
+//6 - Menu de Restaurante:Exiba um menu com 3 opções de prato e peça ao usuário para escolher um número. Exiba o nome do prato escolhido.
+
+//7 - Conversor de Unidades:Peça ao usuário que escolha uma unidade de temperatura (Celsius, Fahrenheit ou Kelvin) e exiba informações sobre a conversão.
+
+//8 - Classificação de Veículo:Peça um tipo de veículo (carro, moto, caminhão) e exiba uma mensagem com a categoria correspondente.
+
+//9 - Estado Civil:Peça ao usuário para inserir seu estado civil (Solteiro, Casado, Divorciado, Viúvo) e exiba uma mensagem correspondente.
+
+//10 - Código de Produto:Peça um código numérico e exiba o nome do produto correspondente (ex: 1 - Arroz, 2 - Feijão, 3 - Leite).
